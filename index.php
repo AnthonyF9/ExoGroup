42b69fe26cab8c2ccba5ed7eb9ba81cdc97b6d6d
Anthony <anthony.fabregat@gmail.com>
2017-12-01 16:35:20 +0100

    test

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 
 
-<p> On fait un reset <p>
-<<<<<<< HEAD
-<p>salut les poulettes </p>
-=======
-
-<<<<<<< HEAD
-<p>Imaginatioooooooooooon !</p>
-=======
-<h2>Je prépare un test pour savoir si un triangle est rectangle</h2>
-<p>Guillaume</p>
->>>>>>> a56109ff5c07b4f7f45ca903ce9afb501f642d54
-<<<<<<< HEAD
-=======
-<p>lollolololo</p>
->>>>>>> 3247a639ca9abdd0df4a3f1a819456050ffd4694
+<p> On fait un reset une deuxième fois <p>
