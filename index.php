125df4d83b9e00a8ccb345b37c5ea02a5536a4da
Guillaume Giudici <gui.giudici@gmail.com>
2017-12-01 15:27:06 +0100

    ajout1 guillaume

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 <h1>Banana !</h1>
 
 
-<h2>Je prépare un test pour vérifier qu'un traingle est rectangle.</h2>
+<h2>Je prépare un test pour vérifier qu'un triangle est rectangle.</h2>
 <p>Guillaume</p>
